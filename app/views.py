342146fe4a6ffc5d75143445ebd3f1da98b81e06
agrant12 <alving.nyc@gmail.com>
2013-04-09 18:07:05 -0400

    Add template pages, url navigation, and stylesheet

--- app/views.py
+++ app/views.py
@@ -4,4 +4,15 @@ from app import app
 
 @app.route('/')
 def index():
-	return 'Lets get Started!'
+	return render_template("index.html",
+		Title = "Home")
+
+@app.route('/explore')
+def explore():
+	return render_template("explore.html",
+		Title = "Explore")
+
+@app.route('/radio')
+def radio():
+	return render_template("radio.html",
+		Title = "Radio")
