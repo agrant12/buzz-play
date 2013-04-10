0c3391b1082ffaa7a05bde44412cd410ae541e1c
Alvin Grant <alving.nyc@gmail.com>
2013-04-10 13:18:09 -0400

    Added css and elements to base to flesh to front end

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, redirect, flash, abort
+from werkzeug import check_password_hash, generate_password_hash
 
 from app import app
 
@@ -16,3 +17,14 @@ def explore():
 def radio():
 	return render_template("radio.html",
 		Title = "Radio")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User(form.username.data, form.email.data,
+                    form.password.data)
+        db_session.add(user)
+        flash('Thanks for registering')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
