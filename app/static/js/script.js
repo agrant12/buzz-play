ccecfc75ad25bfd6cb0f579a3888cb9301b77145
agrant12 <alving.nyc@gmail.com>
2013-04-11 15:52:34 -0400

    Added profile slider function

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,7 +1,14 @@
 jQuery(document).ready(function(){
 	$me = $('.me');
 
-	$me.click(function(){
-		$('header').css({'top' : '100px'});
+	$me.toggle(function(){
+		$('header').animate({'top' : '500px'});
+		$('.profile').animate({'margin-top': '0'});
+	},
+	function() {
+		$('header').animate({'top' : '0px'});
+		$('.profile').animate({'margin-top': '-500px'});
 	});
 });
+
+
